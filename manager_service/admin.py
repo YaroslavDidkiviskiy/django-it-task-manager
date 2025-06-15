29264f3bc1934c5959563ee6a504eeb5f77ec2fb
Yaroslav Didkivskiy <yaroslavdidkivskiy@gmail.com>
2025-06-15 04:25:00 +0300

    Implemented file admin.py and added some validations for models.py.

--- manager_service/admin.py
+++ manager_service/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Worker, Task, TaskType, Position
+
+admin.site.register(Position)
+admin.site.register(TaskType)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = [
+        "name", "description",
+        "deadline", "is_completed",
+        "priority", "task_type",
+        "assignees_list",
+        ]
+    list_filter = ["is_completed", "priority", "task_type"]
+    date_hierarchy = "deadline"
+    ordering = ["-deadline"]
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    add_fieldsets = (
+        *UserAdmin.add_fieldsets,
+        ("Personal info", {"fields": ("first_name", "last_name", "position")}),
+    )
--- manager_service/models.py
+++ manager_service/models.py
@@ -36,19 +36,25 @@ class Worker(AbstractUser):
     position = models.ForeignKey(Position, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return f"{self.username} - {self.position.name}"
-
+        if self.position:
+            return f"{self.username} - {self.position.name}"
+        return f"{self.username} - No Position"
 
 class Task(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     deadline = models.DateField(validators=[MinValueValidator(date.today())])
     is_completed =models.BooleanField(default=False)
     priority = models.CharField(choices=PRIORITY_CHOICES, max_length=10, default="MEDIUM", )
     task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
     assignees = models.ManyToManyField(Worker, related_name="assigned_tasks")
 
+    def assignees_list(self):
+        return ", ".join(worker.username for worker in self.assignees.all())
+    assignees_list.short_description = 'Assignees'
+
     def __str__(self):
+        task_type_name = self.task_type.name if self.task_type else "No Type"
         assignees = ", ".join(worker.username for worker in self.assignees.all())
         status = "✅" if self.is_completed else "❌"
-        return f"{self.name} — {self.get_priority_display()} [{self.task_type.name}] до {self.deadline} | {assignees} {status}"
+        return f"{self.name} — {self.get_priority_display()} [{task_type_name}] до {self.deadline} | {assignees} {status}"
