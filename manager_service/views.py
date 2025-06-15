833fa339b691836fdeb010ce02eabb416e95e577
Yaroslav Didkivskiy <yaroslavdidkivskiy@gmail.com>
2025-06-15 18:42:01 +0300

    Implemented home page and added sidebar, pagination for next features

--- manager_service/views.py
+++ manager_service/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Worker, Task, TaskType, Position
+
+def index(request):
+    num_workers = Worker.objects.count()
+    num_tasks = Task.objects.count()
+    task_list = Task.objects.all()
+    completed_tasks = Task.objects.filter(is_completed=True).count()
+
+    context = {
+        'num_workers': num_workers,
+        'num_tasks': num_tasks,
+        'task_list': task_list,
+        'completed_tasks': completed_tasks,
+    }
+
+    return render(request, "manager_service/index.html", context=context)
