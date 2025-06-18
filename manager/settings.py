2d9b79b92d82b552ab5ae3d2bb931423be8f901f
Yaroslav Didkivskiy <yaroslavdidkivskiy@gmail.com>
2025-06-18 15:58:47 +0300

    added secret key to env

--- manager/settings.py
+++ manager/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9!y6w8z4w7t6(^-5ooz8@ssvsnl+!07n!z$e+2#6-3hd@+o%9)'
+SECRET_KEY = config("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
