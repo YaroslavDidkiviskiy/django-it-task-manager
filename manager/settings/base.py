85d650034a2c8115e54a295bc801d316ee46a3ae
Yaroslav Didkivskiy <yaroslavdidkivskiy@gmail.com>
2025-06-19 17:54:29 +0300

    connected to postgreSQL and upgraded settings

--- manager/settings/base.py
+++ manager/settings/base.py
@@ -9,26 +9,23 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+# SECRET_KEY = config("SECRET_KEY")
 
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-9!y6w8z4w7t6(^-5ooz8@ssvsnl+!07n!z$e+2#6-3hd@+o%9)")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -78,14 +75,6 @@ WSGI_APPLICATION = 'manager.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 CRISPY_TEMPLATE_PACK = "bootstrap3"
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
